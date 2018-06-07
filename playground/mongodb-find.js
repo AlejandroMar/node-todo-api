c02d62401953f98e7c15f7eb16b9af8e7be615b9
Alejo <Alejo@Alejos-MacBook-Pro.local>
2018-06-07 10:28:35 +0200

    fetch for all docs

---
+++ playground/mongodb-find.js
@@ -0,0 +1,26 @@
+//const MongoClient = require('mongodb').MongoClient;
+//The objectID property let's me create and do things with ids
+const {MongoClient, ObjectID} = require('mongodb');
+
+const obj =  new ObjectID;
+//I created an unique id that i can use in many things like for example in react to connect the react id with the mongo id
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('Unable to connect to MondoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+    //The toArray method returns a promise
+    db.collection('Todos').find().toArray()
+        .then(docs => {
+            console.log('Users');
+            console.log(JSON.stringify(docs, undefined, 2))
+        })
+        .catch(err => console.log('Unable to fetch docs', err));
+    //here I don't client close()
+   client.close(); 
+})
